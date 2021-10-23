cb51e9b89eba601920b15c6d9a043d486066dbc3
ak-parser <81529142+ak-parser@users.noreply.github.com>
2021-10-23 21:03:20 +0300

    Create program. Add functionality to AttachentsDepth.cs

---
+++ AttechmentsDepth.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace AttechmentsDepth
+{
+    class AttechmentsDepth
+    {
+        public List<string> Sentence { get; }
+
+        public AttechmentsDepth()
+        {
+            Sentence = new List<string>();
+        }
+
+        public void AddSentenceFromFile(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                bool flagIsNewSentence = true;
+
+                while (!reader.EndOfStream)
+                {
+                    string input = reader.ReadLine();
+
+                    foreach (string elem in input.Split('.', StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        if (flagIsNewSentence == true)
+                            Sentence.Add(elem);
+                        else
+                        {
+                            Sentence[Sentence.Count - 1] += elem;
+                            flagIsNewSentence = true;
+                        }
+                    }
+
+                    if (input[input.Length - 1] != '.')
+                        flagIsNewSentence = false;
+                }
+            }
+        }
+
+        public string MaxAttechmentsDepth(Action<string> OnIncorrectSentence)
+        {
+            int maxDepth = 0;
+            string result = "";
+
+            if (Sentence.Count == 0)
+                throw new ArgumentException("Error when called MaxAttechmentsDepth: there are no sentence. Add sentence using AddSentenceFromFile");
+
+            foreach (string elem in Sentence)
+            {
+                int currMaxDepth = 0, currDepth = 0;
+
+                for (int i = 0; i < elem.Length; i++)
+                {
+                    if (elem[i] == '(' && ++currDepth > currMaxDepth)
+                    {
+                        currMaxDepth = currDepth;
+                    }
+                    else if (elem[i] == ')')
+                    {
+                        currDepth--;
+                    }
+                }
+
+                if (currDepth != 0)
+                {
+                    OnIncorrectSentence?.Invoke(elem);
+                    currDepth = 0;
+                }
+                else if (currMaxDepth > maxDepth)
+                {
+                    maxDepth = currMaxDepth;
+                    result = elem;
+                }
+            }
+
+            return result;
+        }
+
+        public void SortSentence(Comparison<string> comparison)
+        {
+            Sentence.Sort(comparison);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace AttechmentsDepth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AttechmentsDepth attechmentsDepth = new AttechmentsDepth();
+
+            try
+            {
+                //Вважається, що речення закінчується крапкою та може бути розміщене в одному або декількох рядках
+                attechmentsDepth.AddSentenceFromFile("../../../input.txt");
+
+                //Якщо кількість відкриваючих дужок не дорівнює кількості закриваючих, то виводиться повідомлення про цю стрічку на консоль, при цьому пошук не переривається
+                Console.WriteLine("String with max attechments depth:\n" + 
+                    attechmentsDepth.MaxAttechmentsDepth((message) => Console.WriteLine($"Incorrect attechments count in sentence: \"{message}\"")));
+
+                attechmentsDepth.SortSentence((sentence1, sentence2) => sentence1.Length > sentence2.Length ? 1 : (sentence1.Length < sentence2.Length ? -1 : 0));
+                Console.WriteLine("\nSentence sorted by length:");
+                foreach (var elem in attechmentsDepth.Sentence)
+                    Console.WriteLine(elem);
+            }
+            catch(FileNotFoundException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
